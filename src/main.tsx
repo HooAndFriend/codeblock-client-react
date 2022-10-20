641f71d3b1b76c5c899a0b229f1e6d7d0769d846
pino._.hoo <inhoo23@naver.com>
2022-10-20 19:22:40 +0900

    add : react-router-dom, css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App"
+import "./main.css"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <BrowserRouter>
     <App />
+  </BrowserRouter>
 )
